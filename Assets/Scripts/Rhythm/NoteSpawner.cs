95343248728211a64ed7bab1f78c5fa11fb4b577
anaf-4 <kimdongmin12355@gmail.com>
2026-09-22 23:24:48 +0900

    feat: add NoteSpawner

---
+++ Assets/Scripts/Rhythm/NoteSpawner.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class NoteSpawner : MonoBehaviour
+{
+    [SerializeField] private SongConductor conductor;
+    [SerializeField] private JudgmentSystem judgmentSystem;
+    [SerializeField] private NoteView notePrefab;
+    [SerializeField] private Transform[] laneSpawnPoints = new Transform[4];
+    [SerializeField] private Transform[] laneJudgmentPoints = new Transform[4];
+    [SerializeField] private Sprite[] laneNoteSprites = new Sprite[4];
+    [SerializeField] private float leadTimeSeconds = 2f;
+    [SerializeField] private string chartResourcePath = "Charts/perfect_run_test";
+
+    private ChartData chart;
+    private int nextNoteIndex;
+
+    private void Awake()
+    {
+        chart = ChartLoader.LoadFromResources(chartResourcePath);
+    }
+
+    private void Update()
+    {
+        if (chart?.notes == null) return;
+
+        while (nextNoteIndex < chart.notes.Length &&
+               chart.notes[nextNoteIndex].time - leadTimeSeconds <= conductor.SongTimeSeconds)
+        {
+            SpawnNote(chart.notes[nextNoteIndex]);
+            nextNoteIndex++;
+        }
+    }
+
+    private void SpawnNote(NoteData data)
+    {
+        float spawnTime = data.time - leadTimeSeconds;
+        Transform spawnPoint = laneSpawnPoints[data.lane];
+        Transform judgmentPoint = laneJudgmentPoints[data.lane];
+
+        NoteView note = Instantiate(notePrefab, spawnPoint.position, Quaternion.identity);
+        note.Initialize(
+            data.lane, data.time, spawnTime,
+            spawnPoint.position, judgmentPoint.position,
+            conductor, laneNoteSprites[data.lane]);
+
+        judgmentSystem.Register(note);
+    }
+}
